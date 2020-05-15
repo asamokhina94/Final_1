b8eb64f7abded1243bb46b32c53c553a690fb803
asamokhina94 <cdfiY#16Z>
2020-05-15 12:51:44 +0300

    Добавлена проверка на двоичное число

--- src/Final_1.java
+++ src/Final_1.java
@@ -1,21 +1,28 @@
 import java.util.Scanner;
 import java.lang.Math;
 
-public class Final_1
-{
-    public static void main(String[] args){
+public class Final_1 {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Введите число в двоичном формате: ");
         int num = in.nextInt();        //Запись введённого с клавиатуры двоичного числа в переменную
         int result = 0;          //Создаём переменную результата перевода
         int stepen = 0;         //Создаём переменную для возведения в степень числа 2
         int x;
-        while (num!= 0){
-            x = num % 10;          //Получаем остаток от деления введённого числа на 10
-            result += x*Math.pow(2, stepen);        //Остаток от деления умножаем на 2 в степени, складываем с результатом
-            stepen++;           //Степень увеличивается на единицу
-            num = num/10;      //Сокращаем число делением на 10
+        boolean it_binary = true;
+        while (num != 0) {
+            x = num % 10;         //Получаем остаток от деления введённого числа на 10
+            if (x > 1 || x < 0) {
+                System.out.println("Число не является двоичным");
+                it_binary = false;
+                break;
+            } else {
+                result += x * Math.pow(2, stepen);        //Остаток от деления умножаем на 2 в степени, складываем с результатом
+                stepen++;             //Степень увеличивается на единицу
+                num = num / 10;        //Сокращаем число делением на 10
+            }
         }
-        System.out.println("Ваше число в десятичном формате: " + result);
+           if (it_binary){
+            System.out.println("Ваше число в десятичном формате: " + result);}
+        }
     }
-}
